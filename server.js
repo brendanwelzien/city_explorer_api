864b43d12a9a2d225ca5d14a57f7ecd5b18016ea
Brendan Welzien <brendanwelzien@gmail.com>
2020-10-13 15:48:07 -0700

    testing

--- server.js
+++ server.js
@@ -6,9 +6,12 @@ const express = require('express');
 const cors = require('cors');
 const response = require('express');
 const app = express();
+const superagent = require('superagent');
 
 //to accept incoming traffic
 const PORT = process.env.PORT || 3001;
+
+
 
 
 // allows for public server
@@ -20,20 +23,32 @@ app.get('/', (request, response) => {
 });
 
 // functions
-function Location (obj) {
-    this.search_query = obj[0].display_name;
-    this.formatted_query = obj[0].display_name;
-    this.latitude = obj[0].lat;
-    this.longitude = obj[0].lon;
+function Location (locationObj) {
+    this.formatted_query = locationObj[0].display_name;
+    this.latitude = locationObj[0].lat;
+    this.longitude = locationObj[0].lon;
 }
-function Weather(obj2) {
-    this.city = obj2.city_name;
-    this.forecast = obj2.weather.description;
-    this.time = obj2.datetime;
+function Weather(weatherObj) {
+    
+    this.forecast = weatherObj[0].weather.description;
+    this.time = weatherObj[0].datetime;
 }
-// since there is an array, you need to make an instance of the characteristics
-function weatherConfig (request, response){
-    let weatherArr = [];
+
+function Trails(trailObj) {
+    this.name = trailObj.trails[0].name;
+    this.location = trailObj.trails[0].location;
+    this.length = trailObj.trails[0].length;
+    this.stars = trailObj.trails[0].stars;
+    this.star_votes = trailObj.trails[0].starVotes;
+    this.summary = trailObj.trails[0].summary;
+    this.conditions = trailObj.trails[0].conditionStatus;
+    this.condition_date = trailObj.trails[0].conditionDate;
+    this.condition_time = trailObj.trails[0].conditionDetails;
+}
+
+// weather request and response
+
+ /*   let weatherArr = [];
     let obj = require('./data/weather.json');
     obj.data.forEach(label => {
         let weatherStuff = new Weather(label);
@@ -45,26 +60,71 @@ function weatherConfig (request, response){
         response.status(404).send('Sorry, something went wrong,');
     });
 }
+*/
+app.get('/weather', weatherConfig);
+function weatherConfig (request, response){
+const cityData = req.query.city;
+const GEOCODEAPI = process.env.GEOCODE_API_KEY;
+const weatherURL = `https://api.weatherbit.io/v2.0/current?city=${cityData}&key=${GEOCODEAPI}`; 
+
+console.log(weatherURL);
+// superagent process is GET > SET > THEN > RETURN XXXX > CATCH?
+superagent.get(weatherURL)
+
+.set('key', GEOCODEAPI)
+    .then(weatherRetrievalInfo => {
+        const weatherObj = weatherRetrievalInfo.body;
+
+        let weatherCollection = weatherObj.data.map( objIndex => {
+            const weatherMapFcn = new Weather(objIndex);
+            return weatherMapFcn;
+        })
+        response.send(weatherCollection);
+    }); // this is the request / response promise
+}
+
 
 // location request and response
 app.get('/location', (request, response) => {
-    const locationData = require('./data/location.json');
-    const createLocation = new Location(locationData);
-    response.send(createLocation);
-    let cityName = 'Lynnwood';
-    if(request.query.city !== cityName){
-        app.get('/', (request, response) => {
-        response.status(500).send('Oops wrong city');
+    const cityDataTwo = request.query.city;
+    const LOCATIONIQAPI = process.env.LOCATIONIQ_API_KEY;
+    const locationURL = `https://us1.locationiq.com/v1/search.php?key=${LOCATIONIQAPI}&q=${cityDataTwo}`
+    superagent.get(locationURL)
+        .then(locationInfo => {
+            const locationObj = locationInfo.body;
+            const locationNew = new Location(locationObj);
+            response.send(locationNew);
+        })
+        .catch(error => {
+            return response.status(500).send(error);
         });
-    }
-})
+});
 
-// weather request and response
-app.get('/weather', weatherConfig);
+//trails 
+app.get('trails', trailFcn);
+
+function trailFcn(request, response) {
+    const TRAILSAPI = process.env.TRAILS_API_KEY;
+    const lonData = request.query.longitude;
+    const latData = request.query.latitude;
+    const trailsURL = `https://hikingproject.com/data/get-trails?lat=${latData}&lon=${lonData}&maxDistance=30&key=${TRAILSAPI}`;
+    // use superagent method ... .get .then return then response send
+    superagent.get(trailsURL)
+    .set('key', TRAILSAPI)
+        .then(trailData => {
+        const trailObj = trailData.body;
+        
+        let trailCollection = trailObj.trails.map(trailIndex => {
+            const trailNew = new Trails(trailIndex);
+            return trailNew;
+        });
+        response.send(trailCollection);
+    });  
+}
 
 
 // simple error feature
-function handleLocation(request, response) {
+/*function handleLocation(request, response) {
     try {
       // try to "resolve" the following (no errors)
       const geoData = require('./data/location.json');
@@ -76,5 +136,5 @@ function handleLocation(request, response) {
       response.status(500).send('sorry, something broke.');
     }
   }
-
-app.listen(PORT,() => console.log(`Listening on port ${PORT}`));
+*/
+app.listen(PORT);

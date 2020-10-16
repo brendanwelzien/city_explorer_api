e468c1350afb2580c39d6349c644ef5e8b8692e8
Brendan Welzien <brendanwelzien@gmail.com>
2020-10-15 19:26:28 -0700

    had to reconfigure main, heroku was not working properly

--- server.js
+++ server.js
@@ -5,10 +5,7 @@ require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 const app = express();
-const response = require('express');
 const superagent = require('superagent');
-const pg = require('pg');
-
 
 //to accept incoming traffic
 const PORT = process.env.PORT || 3001;
@@ -25,14 +22,12 @@ app.get('/', (request, response) => {
     response.send('homepage');
 });
 
-const client = new pg.Client(dbURL); //connecting to database
-
 // functions
 function Location (locationObj, city) {
     this.search_query = city;
-    this.formatted_query = locationObj.display_name;
-    this.latitude = locationObj.lat;
-    this.longitude = locationObj.lon;
+    this.formatted_query = locationObj[0].display_name;
+    this.latitude = locationObj[0].lat;
+    this.longitude = locationObj[0].lon;
 }
 function Weather(weatherObj) {
     
@@ -89,10 +84,7 @@ superagent.get(weatherURL)
             return weatherMapFcn;
         })
         response.send(weatherCollection);
-    })
-        .catch(error => {
-            response.status(500).send(error);
-        }); // this is the request / response promise
+    }); // this is the request / response promise
 }
 
 
@@ -113,70 +105,13 @@ app.get('/location', (request, response) => {
             
             return response.status(500).send(error);
         });
-    
-
-    // RECEIVED HELP FROM TA CHANCE IN LAB 7 / 8
-app.get('/location', (request, response) => {
-
-    const selectSQL = 'SELECT * FROM location;';
-    const cityDataTwo = request.query.city;
-
-    console.log(request.query.city);
-    client.query(selectSQL)
-
-
-        .then(dataSQL => {
-            let grabValues = dataSQL.rows.map(city => city.search_query.toLowerCase());
-            console.log(grabValues);
-
-            if(grabValues.includes(cityDataTwo)){
-                client.query(`SELECT * FROM location WHERE search_query = '${cityDataTwo}'`)
-
-                .then(placeStuff => {
-                    response.send(placeStuff.rows[0]);
-                });
-            } else {
-                const locationURL = `https://us1.locationiq.com/v1/search.php?key=${LOCATIONIQAPI}&q=${cityDataTwo}&format=json`;
-                console.log('in superagent');
-
-                superagent.get(locationURL)
-                .then(sqlConfig => {
-                    const sqlArr = sqlConfig.body[0];
-
-
-                    console.log(sqlArr); // (locationObj, city)
-                    const locationNew = new Location(sqlArr, cityDataTwo);
-                    //response.send(locationNew);
-                    const cityName = locationNew.search_query;
-                    const cityInfo = locationNew.formatted_query;
-
-                    const cityLat = parseFloat(locationNew.latitude);
-
-                    const cityLon = parseFloat(locationNew.longitude);
-
-                    const cityString = 'INSERT INTO location (search_query, formatted_query, latitude, longitude) VALUES ($1, $2, $3, $4)';
-
-                    const cityLayout = [cityName, cityInfo, cityLat, cityLon];
-                    client.query(cityString, cityLayout)
-                     // this utilizes crud / rest with formatting
-                     .then (() => {
-                         response.status(200).send(locationNew);
-                     })
-                     .catch(error => {
-                         response.status(500).send(error);
-                });
-            })
-           .catch(error => {
-                response.status(500).send(error); // error is for retrieving location
-            });
-        }
 });
-});
+
 //trails 
 app.get('/trails', trailFcn);
 
 function trailFcn(request, response) {
-   // const TRAILSAPI = process.env.TRAILS_API_KEY;
+    const TRAILSAPI = process.env.TRAILS_API_KEY;
     const lonData = request.query.longitude;
     const latData = request.query.latitude;
     const trailsURL = `https://hikingproject.com/data/get-trails?lat=${latData}&lon=${lonData}&maxDistance=30&key=${TRAILSAPI}`;
@@ -192,9 +127,6 @@ function trailFcn(request, response) {
             return trailNew;
         });
         response.send(trailCollection);
-    })
-    .catch(error => {
-        response.status(500).send(error.message);
     });  
 }
 
@@ -252,9 +184,5 @@ function movieConfig(request, response) {
 
 
 app.listen(PORT, () => {
-    console.log(`server is up on ${PORT}`);
+    console.log(`${PORT}`)
 });
-})
-.catch( error => {
-    console.error('connection error', error);
-}) // taken from lecture example
